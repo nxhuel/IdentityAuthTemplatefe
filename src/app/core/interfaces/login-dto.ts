d7a5711e6c02a72fd9805c9efaeb8a8efb6024be
Nahuel Tisera <nahueltisera03@gmail.com>
2025-07-24 19:30:46 -0300

    identityauthtemplatefe: inicio de plantilla

---
+++ src/app/core/interfaces/login-dto.ts
@@ -0,0 +1,4 @@
+export interface LoginDto {
+    UserName: string;
+    Password: string;
+}
---
+++ src/app/core/interfaces/register-dto.ts
@@ -0,0 +1,5 @@
+export interface RegisterDto {
+    UserName: string;
+    Password: string;
+    Email: string;
+}
